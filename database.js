6d0dde2c5e8a7ebff166bef742f777d9b5b7ae89
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-07-19 00:15:56 -0500

    refactor database queries into a separate file

---
+++ database.js
@@ -0,0 +1,186 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { playerTemplate } = require('./mongoTemplates');
+const badWords = require('./badwords');
+
+const mongoDB = new MongoClient(process.env.mongoConnectionStr, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+const whichDatabase = process.env.isDev == "yes" ? "takepoint-dev" : "takepoint";
+const db = mongoDB.db(whichDatabase);
+
+const collections = {
+    reservedUsers: db.collection("reservedUsers"),
+    players: db.collection("players"),
+    sessions: db.collection("sessions"),
+    games: db.collection("games")
+};
+
+const queries = {
+    async register(username, password, email) {
+        let usernameLower = username.toLowerCase();
+        let emailLower = email.toLowerCase();
+        let resp = await collections.players.findOne({ 
+            $or: [{ usernameLower }, { email: emailLower }]
+        });
+
+        if (resp) {
+            if (resp.allowReregister && resp.usernameLower === usernameLower && resp.email === emailLower) {
+                let hash = await bcrypt.hash(password, 8);
+                resp.passwordHash = hash;
+                resp.email = emailLower;
+                resp.allowReregister = false;
+                await collections.players.replaceOne({ username: resp.username }, resp);
+                return { error: false, username: resp.username };
+            } else {
+                if (resp.usernameLower === usernameLower) {
+                    return { error: true, desc: "A player with that username already exists!", code: 1 };
+                } else if (resp.email === emailLower) {
+                    return { error: true, desc: "A player with that email already exists!", code: 2 };
+                }
+                return { error: true, desc: "Generic" };
+            }
+        }
+
+        let isReserved = await collections.reservedUsers.findOne({ usernameLower });
+        if (isReserved) {
+            return { error: true, desc: 'That username is reserved as it belongs to a notable player. Check the Discord for help.', code: 1 };
+        }
+
+        for (let word of badWords) {
+            if (usernameLower.includes(word)) {
+                return { error: true, desc: 'Username may include profanity. Check the Discord for help.', code: 1 };
+            }
+        }
+
+        let hash = await bcrypt.hash(password, 8);
+        let player = playerTemplate({
+            username,
+            email: emailLower,
+            passwordHash: hash
+        });
+        await collections.players.insertOne(player);
+        return { error: false, username };
+    },
+
+    async login(usernameEmail, password) {
+        let usernameEmailLower = usernameEmail.toLowerCase();
+        let player = await collections.players.findOne({
+            $or: [{ usernameLower: usernameEmailLower }, { email: usernameEmailLower }]
+        });
+
+        if (player && bcrypt.compareSync(password, player.passwordHash)) {
+            return { error: false, username: player.username, perms: player.perms || 0 };
+        } else {
+            return { error: true, desc: "The provided username and password does not exist in our database.", code: 0 };
+        }
+    },
+
+    async setSession(username, cookie) {
+        let res = await collections.sessions.updateOne(
+            { username },
+            { $set: { cookie } },
+            { upsert: true }
+        );
+
+        if (res.modifiedCount) {
+            return { error: false };
+        } else {
+            return { error: true };
+        }
+    },
+
+    async resumeSession(cookie) {
+        let res = await collections.sessions.findOne({ cookie });
+        if (!res) return { error: true };
+
+        let player = await collections.players.findOne({ username: res.username });
+        return { error: false, username: res.username, perms: player.perms || 0 };
+    },
+
+    async insertGame(username, stats) {
+        stats.username = username;
+        await collections.games.insertOne(stats);
+    },
+
+    async updateStats(username, stats) {
+        let player = await collections.players.findOne({ usernameLower: username.toLowerCase() });
+        if (!player) return;
+
+        let weaponList = ["pistol", "assault", "sniper", "shotgun"];
+        let perkList = ["barrier", "health", "gas", "frag", "turret", "sd"];
+        
+        player.score += stats.score;
+        funFacts['Score gained'].incrementValue(stats.score);
+        player.timePlayed += stats.timeAlive;
+        player.spm = parseFloat((player.score / (player.timePlayed / 1000 / 60)).toFixed(2));
+        player.pointsTaken += stats.pointsTaken;
+        player.pointsNeutralized += stats.pointsNeutralized;
+        player.kills += stats.kills;
+        funFacts['Kills'].incrementValue(stats.kills);
+        if (stats.kills > player.killstreak) player.killstreak = stats.kills;
+        player.deaths++;
+        player.kdr = parseFloat((player.kills / player.deaths).toFixed(2));
+        player.shotsFired += stats.bulletsFired;
+        player.shotsHit += stats.bulletsHit;
+        player.accuracy = parseFloat((100 * player.shotsHit / player.shotsFired).toFixed(2));
+        player.damageDealt += stats.damageDealt;
+        funFacts['Damage dealt'].incrementValue(stats.damageDealt);
+        player.distanceCovered += Math.round(stats.distanceCovered);
+        player.doubleKills += stats.doubleKills;
+        player.tripleKills += stats.tripleKills;
+        player.multiKills += stats.multiKills;
+        
+        let pistol = player.weapons.pistol;
+        let pistolUpdates = stats.weapons[0];
+        pistol.kills += pistolUpdates.kills;
+        funFacts['Kills with pistol'].incrementValue(pistolUpdates.kills);
+        pistol.shotsFired += pistolUpdates.bulletsFired;
+        pistol.shotsHit += pistolUpdates.bulletsHit;
+        pistol.damageDealt += pistolUpdates.damageDealt;
+        
+        if (stats.weaponChosenID !== undefined) {
+            let id = stats.weaponChosenID;
+            let weaponName = weaponList[id];
+            let weaponStats = player.weapons[weaponName];
+            weaponStats.kills += stats.weapons[id].kills;
+            funFacts['Kills with ' + weaponName].incrementValue(stats.weapons[id].kills);
+            weaponStats.shotsFired += stats.weapons[id].bulletsFired;
+            weaponStats.shotsHit += stats.weapons[id].bulletsHit;
+            weaponStats.accuracy = parseFloat((100 * weaponStats.shotsHit / weaponStats.shotsFired).toFixed(2));
+            weaponStats.damageDealt += stats.weapons[id].damageDealt;
+            weaponStats.selected++;
+            weaponStats.timePlayed += Date.now() - stats.weaponChosenTime;
+            
+            if (!weaponStats.attachments) weaponStats.attachments = { 1: 0, 2: 0 };
+            if (stats.attachmentID) weaponStats.attachments[stats.attachmentID]++;
+            pistol.timePlayed += stats.weaponChosenTime - stats.spawnTime;
+        } else {
+            pistol.timePlayed += Date.now() - stats.spawnTime;
+        }
+
+        funFacts['Hours played'].incrementValue((Date.now() - stats.spawnTime) / (60 * 60 * 1000));
+        player.upgrades.speed += stats.upgrades.speed;
+        player.upgrades.reload += stats.upgrades.reload;
+        player.upgrades.mags += stats.upgrades.mags;
+        player.upgrades.view += stats.upgrades.view;
+        player.upgrades.heal += stats.upgrades.heal;
+        
+        if (stats.perkID) player.perks[perkList[stats.perkID - 1]]++;
+        
+        player.lastActive = Date.now();
+        await collections.players.replaceOne({ username }, player);
+    },
+
+    async deleteOldGames(olderThan) {
+        let oldestTs = Date.now() - olderThan * (24 * 60 * 60 * 1000);
+        await collections.games.deleteMany({ spawnTime: { $lt: oldestTs } });
+    }
+};
+
+module.exports = queries;
--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 require('dotenv').config();
 const axios = require('axios');
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require('cors');
 const express = require('express');
-const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const Server = require("./server");
 const Cloudflare = require('./cloudflare');
-const { playerTemplate } = require('./mongoTemplates');
-const badWords = require('./badwords');
+const db = require('./database');
 
 const app = express();
 const CFWorker = new Cloudflare(process.env.cloudflareZoneID, process.env.cloudflareAPIKey, process.env.cloudflareAPIEmail);
-const mongoDB = new MongoClient(process.env.mongoConnectionStr, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-const whichDatabase = process.env.isDev == "yes" ? "takepoint-dev" : "takepoint";
-const db = mongoDB.db(whichDatabase);
-const collections = {
-    reservedUsers: db.collection("reservedUsers"),
-    players: db.collection("players"),
-    sessions: db.collection("sessions"),
-    games: db.collection("games")
-};
+
 const sessions = new Map();
 const sessionTimeout = 60;
 
@@ -100,167 +83,13 @@ function isServerAuthorized(key) {
 async function generateSession(username) {
     let sessionCookie = generateCookie();
     sessions.set(username, { lastRefresh: Date.now(), cookie: sessionCookie });
-    let res = await queryDb({ type: "setSession", data: { username: username, cookie: sessionCookie } })
+    let res = await db.setSession(username, sessionCookie);
     if (!res.error) return true;
     else return false;
 }
 
 function generateCookie() {
     return crypto.randomBytes(64).toString('hex');
-}
-
-async function queryDb(query) {
-    let data = query.data;
-    switch (query.type) {
-        case "register": {
-            let resp = await collections.players.findOne({ 
-                $or: [{ usernameLower: data.username.toLowerCase() }, { email: data.email.toLowerCase() }]
-            });
-            if (resp) {
-                if (resp.allowReregister && resp.usernameLower == data.username.toLowerCase() && resp.email == data.email.toLowerCase()) {
-                    let hash = await bcrypt.hash(data.password, 8);
-                    resp.passwordHash = hash;
-                    resp.email = data.email.toLowerCase();
-                    resp.allowReregister = false;
-                    await collections.players.replaceOne({ username: resp.username }, resp);
-                    return { error: false, username: resp.username };
-                }
-                else {
-                    if (resp.usernameLower == data.username.toLowerCase()) {
-                        return { error: true, desc: "A player with that username already exists!", code: 1 };
-                    } else if (resp.email == data.email.toLowerCase()) {
-                        return { error: true, desc: "A player with that email already exists!", code: 2 };
-                    }
-                    return { error: true, desc: "Generic" };
-                }
-            }
-            let isReserved = await collections.reservedUsers.findOne({
-                usernameLower: data.username.toLowerCase()
-            });
-            if (isReserved) {
-                return { error: true, desc: 'That username is reserved as it belongs to a notable player. Check the Discord for help.', code: 1 };
-            }
-            for (word of badWords) {
-                if (data.username.toLowerCase().includes(word)) {
-                    return { error: true, desc: 'Username may include profanity. Check the Discord for help.', code: 1 };
-                }
-            }
-            let hash = bcrypt.hash(data.password, 8);
-            let player = playerTemplate({
-                username: data.username,
-                email: data.email.toLowerCase(),
-                passwordHash: hash
-            });
-            await collections.players.insertOne(playerTemplate(player));
-            return { error: false, username: data.username };
-        }
-        case "login": {
-            let player = await collections.players.findOne({
-                $or: [ { usernameLower: data.usernameEmail.toLowerCase() }, { email: data.usernameEmail.toLowerCase() } ]
-            });
-            console.log(player.passwordHash)
-            if (player && bcrypt.compareSync(data.password, player.passwordHash)) {
-                return { error: false, username: player.username, perms: player.perms || 0 };
-            }
-            else return { error: true, desc: "The provided username and password does not exist in our database.", code: 0 };
-        }
-        case "setSession": {
-            let res = await collections.sessions.updateOne(
-                { username: data.username },
-                { $set: { 
-                    cookie: data.cookie
-                } },
-                { upsert: true }
-            )
-            if (res.modifiedCount) return { error: false };
-            else return { error: true };
-        }
-        case "resumeSession": {
-            let res = await collections.sessions.findOne({ cookie: data.cookie });
-            if (!res) return { error: true };
-            let player = await collections.players.findOne({ username: res.username });
-            return { error: false, username: res.username, perms: player.perms || 0 };
-        }
-        case "insertGame": {
-            data.stats.username = data.username;
-            await collections.games.insertOne(data.stats);
-            return;
-        }
-        case "updateStats": {
-            let stats = data.stats;
-            let username = data.username;
-            let player = await collections.players.findOne({
-                usernameLower: username.toLowerCase()
-            });
-            if (!player) return;
-            let weaponList = ["pistol", "assault", "sniper", "shotgun"];
-            let perkList = ["barrier", "health", "gas", "frag", "turret", "sd"];
-            player.score += stats.score;
-            funFacts['Score gained'].incrementValue(stats.score);
-            player.timePlayed += stats.timeAlive;
-            player.spm = parseFloat((player.score / (player.timePlayed / 1000 / 60)).toFixed(2))
-            player.pointsTaken += stats.pointsTaken;
-            player.pointsNeutralized += stats.pointsNeutralized;
-            player.kills += stats.kills;
-            funFacts['Kills'].incrementValue(stats.kills);
-            if (stats.kills > player.killstreak) player.killstreak = stats.kills;
-            player.deaths++;
-            player.kdr = parseFloat((player.kills / player.deaths).toFixed(2));
-            player.shotsFired += stats.bulletsFired;
-            player.shotsHit += stats.bulletsHit;
-            player.accuracy = parseFloat((100 * player.shotsHit / player.shotsFired).toFixed(2));
-            player.damageDealt += stats.damageDealt;
-            funFacts['Damage dealt'].incrementValue(stats.damageDealt);
-            player.distanceCovered += Math.round(stats.distanceCovered);
-            player.doubleKills += stats.doubleKills;
-            player.tripleKills += stats.tripleKills;
-            player.multiKills += stats.multiKills;
-            let pistol = player.weapons.pistol;
-            let pistolUpdates = stats.weapons[0];
-            pistol.kills += pistolUpdates.kills;
-            funFacts['Kills with pistol'].incrementValue(pistolUpdates.kills);
-            pistol.shotsFired += pistolUpdates.bulletsFired;
-            pistol.shotsHit += pistolUpdates.bulletsHit;
-            pistol.damageDealt += pistolUpdates.damageDealt;
-            if (stats.weaponChosenID) {;
-                let id = stats.weaponChosenID;
-                let weaponName = weaponList[id];
-                let weaponStats = player.weapons[weaponName];
-                weaponStats.kills += stats.weapons[id].kills;
-                funFacts['Kills with ' + weaponName].incrementValue(stats.weapons[id].kills);
-                weaponStats.shotsFired += stats.weapons[id].bulletsFired;
-                weaponStats.shotsHit += stats.weapons[id].bulletsHit;
-                weaponStats.accuracy = parseFloat((100 * weaponStats.shotsHit / weaponStats.shotsFired).toFixed(2));
-                weaponStats.damageDealt += stats.weapons[id].damageDealt++;
-                weaponStats.selected++;
-                weaponStats.timePlayed += Date.now() - stats.weaponChosenTime;
-                if (!weaponStats.attachments) weaponStats.attachments = { 1: 0, 2: 0 };
-                if (stats.attachmentID) weaponStats.attachments[stats.attachmentID]++;
-                pistol.timePlayed += stats.weaponChosenTime - stats.spawnTime;
-            } else {
-                pistol.timePlayed += Date.now() - stats.spawnTime;
-            }
-            funFacts['Hours played'].incrementValue((Date.now() - stats.spawnTime) / (60 * 60 * 1000));
-            player.upgrades.speed += stats.upgrades.speed;
-            player.upgrades.reload += stats.upgrades.reload;
-            player.upgrades.mags += stats.upgrades.mags;
-            player.upgrades.view += stats.upgrades.view;
-            player.upgrades.heal += stats.upgrades.heal;
-            if (stats.perkID) player.perks[perkList[stats.perkID - 1]]++;
-            player.lastActive = Date.now();
-            await collections.players.replaceOne(
-                { username: username },
-                player
-            );
-            return;
-        }
-        case "deleteOldGames": {
-            let maxDays = data.olderThan;
-            let oldestTs = Date.now() - maxDays * (24 * 60 * 60 * 1000);
-            await collections.games.deleteMany({ spawnTime: { $lt: oldestTs } });
-            return;
-        }
-    }
 }
 
 async function getServerAttrs(ipv4) {
@@ -351,18 +180,8 @@ app.post('/gameStats', async (req, res) => {
             break;
         }
     }
-    await queryDb({
-        type: "insertGame",
-        data: {
-            username, stats
-        }
-    });
-    if (username != "N/A") await queryDb({
-        type: "updateStats",
-        data: {
-            username, stats
-        }
-    });
+    await db.insertGame(username, stats);
+    if (username != "N/A") await db.updateStats(username, stats);
     res.status(200);
     res.end();
 });
@@ -370,6 +189,7 @@ app.post('/gameStats', async (req, res) => {
 app.post('/auth/*', async (req, res) => {
     let body = req.body;
     if (!isServerAuthorized(body.auth.registerKey) || !servers.find(e => e.id == body.auth.id)) return;
+    let data = body.data;
     let loc = req.url.split("/")[2];
     if (loc == "updateSessions") {
         let sessionsOnServer = JSON.parse(body.data.sessions);
@@ -382,14 +202,8 @@ app.post('/auth/*', async (req, res) => {
         }
     }
     else if (loc == "register") {
-        let resp = await queryDb({ 
-            type: "register", 
-            data: {
-                username: body.data.username,
-                email: body.data.email,
-                password: body.data.password
-            }
-        });
+        let { username, email, password } = data;
+        let resp = await db.register(username, email, password);
         if (!resp.error) {
             let res = await generateSession(resp.username);
             if (res) resp.cookie = sessions.get(resp.username).cookie;
@@ -400,13 +214,8 @@ app.post('/auth/*', async (req, res) => {
         return;
     }
     else if (loc == "login") {
-        let resp = await queryDb({ 
-            type: "login", 
-            data: {
-                usernameEmail: body.data.usernameEmail,
-                password: body.data.password
-            }
-        });
+        let { usernameEmail, password } = data;
+        let resp = await db.login(usernameEmail, password);
         if (!resp.error) {
             if (sessions.has(resp.username)) {
                 resp.error = true;
@@ -422,12 +231,7 @@ app.post('/auth/*', async (req, res) => {
         res.end();
     }
     else if (loc == "loginCookie") {
-        let resp = await queryDb({ 
-            type: "resumeSession", 
-            data: {
-                cookie: body.data.cookie
-            }
-        });
+        let resp = await db.resumeSession(data.cookie);
         if (!resp.error) {
             if (sessions.has(resp.username)) {
                 resp.error = true;
@@ -441,7 +245,7 @@ app.post('/auth/*', async (req, res) => {
         res.end();
     }
     else if (loc == "logout") {
-        sessions.delete(body.data.username);
+        sessions.delete(data.username);
         res.status(200);
         res.end();
     }
@@ -463,12 +267,7 @@ const loops = {
             initFunFacts();
             leaderboard = [];
             leaderboardDate = getCurrentDate(-5);
-            queryDb({
-                type: "deleteOldGames",
-                data: {
-                    olderThan: 7
-                }
-            });
+            db.deleteOldGames(7);
         }
     }, 5000)
 };
